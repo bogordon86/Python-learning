6a4ed45aad9b1e11af4cdbab333f797b85a55747
Benjamin Gordon <bogordon86@gmail.com>
2017-11-14 18:37:01 -0500

    commit changes

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,11 +1,22 @@
 #import os allows the file to run across systems
 import os
+import re
 
 #open the file and analyze it
+
 paragraph = open("paragraph_1.txt")
+value = paragraph
+def word_count(value):
+    # Find all non-whitespace patterns.
+    list = re.findall("(\S+)", value)
+    # Return length of resulting list.
+    return len(list)
 
-#print out this file
-print (paragraph.read())
+print (word_count(value))
 
+#approximate sentence count
 
+#Average letter count per word
+
+#average sentence length
 
--- PyPoll/main_2.py
+++ PyPoll/main_2.py
@@ -9,8 +9,8 @@ import csv
 filename = "election_data_1.csv"
 
 #Declaring empty dictionary
-candidates = {}
 
+candidates = {}
 # reading csv file
 with open("election_data_1.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -20,28 +20,40 @@ with open("election_data_1.csv", 'r') as csvfile:
     count = (len(open("election_data_1.csv").readlines())-1)
     print(total + str(count))
 
-    for row in csvreader:
 
-#Find the Candidates Values and Number of Votes
+
+    for row in csvreader:
         if row[2] not in candidates:
             candidates[row[2]] = 0
 
         candidates[row[2]] += 1
 
-        #while candidates:
-        candidate_percent = sum((candidates[row[2]])/count)*100
+        print(candidates)
 
-    print (candidates)
+        candidate_name = row[2]
 
+#Find the Candidates Values and Number of Votes
+        if candidate_name not in candidates:
 
+        # Add it to the list of candidates in the running
+            candidates.append(candidate_name)
+        # And begin tracking that candidate's voter count
+            candidate_votes[candidate_name] = 0
+    # Then add a vote to that candidate's count
+            candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+            votes = candidate_votes.get(candidate_name)
+            vote_percentage = (votes / count) * 100
+        print (vote_percentage)
 #The winner of the election based on popular vote.
-    Winning_candidate = 0
-    WC_name = ''
 
-    for name, votes in candidates.items() :
-        if votes > Winning_candidate:
-            Winning_candidate = votes
-            WC_name = name
+Winning_candidate = 0
+WC_name = ''
+
+for name, votes in candidates.items() :
+    if votes > Winning_candidate:
+        Winning_candidate = votes
+        WC_name = name
 
     print("Winner: " + WC_name + " (" + str(Winning_candidate) +")")
 
