f26e69eae5f6d6e8c9f516481ba847a696736e7d
Benjamin Gordon <bogordon86@gmail.com>
2017-11-14 12:46:11 -0500

    commit recent changes

--- PyPoll/main_2.py
+++ PyPoll/main_2.py
@@ -15,7 +15,14 @@ candidates = {}
 with open("election_data_1.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next (csvreader, None)
+    total = "Total Votes: "
+    count = (len(open("election_data_1.csv").readlines())-1)
+    print(total + str(count))
     for row in csvreader:
+
+#Print out the total number of votes
+
+#Find the Candidates Values and Number of Votes
         if row[2] not in candidates:
             candidates[row[2]] = 0
 
@@ -23,10 +30,7 @@ with open("election_data_1.csv", 'r') as csvfile:
 
     print(candidates)
 
-#The total number of votes cast
-        #total = "Total Votes: "
-       # total_votes = len(voter_id)
-   # print (total + str(total_votes))
+
     
 #A complete list of candidates who received votes
     #used = set()
@@ -36,5 +40,13 @@ with open("election_data_1.csv", 'r') as csvfile:
 #The total number of votes each candidate won
 
 #The winner of the election based on popular vote.
+#nner = 0
+   #winner_name = ''
 
+   #for name, votes in candidates.items() :
+       #if votes > winner:
+          # winner = votes
+           #winner_name = name
+
+   #print("Winner: " + winner_name + " (" + str(winner) +")")
 #Winner equals the largest number from the total number of votes
