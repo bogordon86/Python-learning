dee13100844b1fc798f6d9e33494cb2a843fb5b8
Benjamin Gordon <bogordon86@gmail.com>
2017-11-14 19:44:11 -0500

    commit new changes

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -3,20 +3,33 @@ import os
 import re
 
 #open the file and analyze it
+file = 'paragraph_1.txt'
 
-paragraph = open("paragraph_1.txt")
-value = paragraph
-def word_count(value):
-    # Find all non-whitespace patterns.
-    list = re.findall("(\S+)", value)
-    # Return length of resulting list.
-    return len(list)
+# Open the file in "read" mode ('r') and store the contents in the variable "text"
+with open(file, 'r') as text:
 
-print (word_count(value))
+    # Store all of the text inside a variable called "lines"
+    lines = text.read()
+
+    # Print the contents of the text file
+    print(lines)
+
+#approximate word count
+output1 = re.findall("\s+", lines)
+word_count = "Approximate Word Count: "
+print(word_count + str(len(output1)))
 
 #approximate sentence count
+output2 = re.findall("[?.!]", lines)
+sentence_count = "Approximate Sentence Count: "
+print(sentence_count + str(len(output2)))
 
 #Average letter count per word
+words = lines.split()
+average = sum(len(word) for word in words) / len(words)
+average_letter_count = "Average Letter Count: "
+print(average_letter_count + str(average))
 
 #average sentence length
 
+#average_sentence = "Average Sentence Length: "
