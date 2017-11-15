8a781be83f7ccbd531af1022c6b725a8c625ccb9
Benjamin Gordon <bogordon86@gmail.com>
2017-11-14 23:40:31 -0500

    commit

--- PyPoll/main_2.py
+++ PyPoll/main_2.py
@@ -6,55 +6,59 @@ import os
 import csv
  
 # csv file name
-filename = "election_data_1.csv"
+files = "election_data_1.csv"
 
-#Declaring empty dictionary
-
+#Declaring Dictionary
 candidates = {}
+
 # reading csv file
 with open("election_data_1.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next (csvreader, None)
+
 #Calculate Total Votes for each Candidate and Print
     total = "Total Votes: "
     count = (len(open("election_data_1.csv").readlines())-1)
     print(total + str(count))
 
-
-
+    candidates = {}
+    candidates_2 = {}
     for row in csvreader:
+
         if row[2] not in candidates:
             candidates[row[2]] = 0
 
         candidates[row[2]] += 1
+    print(candidates)
+        # The winner of the election based on popular vote.
+    for row in csvreader:
+        county = row[1]
+        candidate = row[2]
 
-        print(candidates)
+        # Find the Candidates Values and Number of Votes
+        if candidate not in candidates_2:
+            candidates_2[candidate] = 0
 
-        candidate_name = row[2]
+        candidates_2[candidate] += 1
 
-#Find the Candidates Values and Number of Votes
-        if candidate_name not in candidates:
+        # Divide count by total items, make percent
+        # aka accessing everything in the dictionary
+        for key, candidate in candidates_2.items():
+            # declare percent as a int
+            percent = 0
+        # Divide value by the total
+        percent = value / count
+        # Format as percent
+        percent = '{0:.0f}%'.format(percent * 100)
+        # key = candidate, value = vote count for candidate, percent
+    print(str(candidate) + str(value) + str(percent))
 
-        # Add it to the list of candidates in the running
-            candidates.append(candidate_name)
-        # And begin tracking that candidate's voter count
-            candidate_votes[candidate_name] = 0
-    # Then add a vote to that candidate's count
-            candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+winner = 0
+winner_name = ''
 
-            votes = candidate_votes.get(candidate_name)
-            vote_percentage = (votes / count) * 100
-        print (vote_percentage)
-#The winner of the election based on popular vote.
+for name, votes in candidates.items():
+    if votes > winner:
+        winner = votes
+        winner_name = name
 
-Winning_candidate = 0
-WC_name = ''
-
-for name, votes in candidates.items() :
-    if votes > Winning_candidate:
-        Winning_candidate = votes
-        WC_name = name
-
-    print("Winner: " + WC_name + " (" + str(Winning_candidate) +")")
-
-
+    print("Winner: " + winner_name + " (" + str(winner) + ")")
